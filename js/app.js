9d862645794e1a0c6d0d425e817ce7203929b9b6
Monica <tamang.monica@gmail.com>
2021-04-09 01:59:49 -0600

    Sent a GET request to recieve activities based on the category, added comments, working on configuring a GET request based on the price range

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ function randomActivitySuccess(res) {
 
 // Creating a function that is called if the page errors, printing an error message to the user
 function randomActivityFailure(err) {
-    document.getElementById(`randomActivityContainer`).innerHTML += `Sorry, something went wrong. Failure to receive activity`;
+    document.getElementById(`randomActivityContainer`).innerHTML += `Sorry, something went wrong. Failed to receive activity. Please try again.`;
 }
 
 // Creating a function that is called when the user clicks on the button
@@ -49,7 +49,7 @@ function participantsSuccess(res) {
 
 // Creating a function that is called if the page errors, printing an error message to the user
 function participantsFailure(err) {
-    document.getElementById(`participantsContainer`).innerHTML += `Sorry, something went wrong. Failure to receive activity.`;
+    document.getElementById(`participantsContainer`).innerHTML += `Sorry, something went wrong. Failed to receive activity. Please try again.`;
 }
 
 // Creating a function that is called when the user clicks on the button
@@ -74,3 +74,46 @@ let participantsSelection = document.getElementById(`participantsSelection`);
 // Adding a click event to the get activity button and calling the function that will send a GET request and print the activity to the user based on the number of participants they have selected
 let getActivityButton = document.getElementById(`getActivityButton`);
 getActivityButton.addEventListener(`click`, getParticipantActivity);
+
+// ---------- BONUS: CATEGORIES ----------
+
+// Creating a function that is called if the network is done and there are no errors
+function categorySuccess(res) {
+
+    // Storing the returned JavaScript object into a variable, printing the activity to the user based on the user's category selection
+    let categoryActivities = res.data;
+    document.getElementById(`categoryActivityResult`).innerText = categoryActivities.activity;
+}
+
+// Creating a function that is called if the page errors, printing an error message to the user
+function categoryFailure(err) {
+    document.getElementById(`categoryContainer`).innerHTML += `Sorry, something went wrong. Failed to receive activity. Please try again.`;
+}
+
+// Creating a function that is called when the user clicks on the button
+function getActivityBasedOnCategory(e) {
+
+    // Getting all the inputs that have the same class name and checking to see if the input is checked
+    let category = document.getElementsByClassName(`categoryInput`);
+    for (let i = 0; i < category.length; i++) {
+
+        // If the input is checked, configure the request with the type and URL with the value of the parameter being the input the user has selected
+        // If the request doesn't fail, it will call the categorySuccess function
+        // If the request fails, it will call the categoryFailure function
+        if (category[i].checked) {
+            axios.request({
+                method: `GET`,
+                url: `http://www.boredapi.com/api/activity`,
+                params: {
+                    type: category[i].value
+                }
+            }).then(categorySuccess).catch(categoryFailure);
+        }
+    }
+}
+
+// Adding a click event to the category activity button and calling the function that will send a GET request and print the activity to the user based the user's category selection
+let categoryActivityButton = document.getElementById(`categoryActivityButton`);
+categoryActivityButton.addEventListener(`click`, getActivityBasedOnCategory);
+
+// ---------- BONUS: PRICE RANGES ----------
