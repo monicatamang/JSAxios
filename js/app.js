9bf4af84d1765f7b20629c71c00c918084be3c2f
Monica <tamang.monica@gmail.com>
2021-04-09 19:04:26 -0600

    Configured a GET request that will print activities based on the user's price range and accessibility, added comments

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ function randomActivitySuccess(res) {
 
 // Creating a function that is called if the page errors, printing an error message to the user
 function randomActivityFailure(err) {
-    document.getElementById(`randomActivityContainer`).innerHTML += `Sorry, something went wrong. Failed to receive activity. Please try again.`;
+    document.getElementById(`randomActivity`).innerText = `Sorry, something went wrong. Failed to receive activity. Please try again.`;
 }
 
 // Creating a function that is called when the user clicks on the button
@@ -49,7 +49,7 @@ function participantsSuccess(res) {
 
 // Creating a function that is called if the page errors, printing an error message to the user
 function participantsFailure(err) {
-    document.getElementById(`participantsContainer`).innerHTML += `Sorry, something went wrong. Failed to receive activity. Please try again.`;
+    document.getElementById(`participantsActivityResult`).innerText = `Sorry, something went wrong. Failed to receive activity. Please try again.`;
 }
 
 // Creating a function that is called when the user clicks on the button
@@ -87,7 +87,7 @@ function categorySuccess(res) {
 
 // Creating a function that is called if the page errors, printing an error message to the user
 function categoryFailure(err) {
-    document.getElementById(`categoryContainer`).innerHTML += `Sorry, something went wrong. Failed to receive activity. Please try again.`;
+    document.getElementById(`categoryActivityResult`).innerText = `Sorry, something went wrong. Failed to receive activity. Please try again.`;
 }
 
 // Creating a function that is called when the user clicks on the button
@@ -117,3 +117,137 @@ let categoryActivityButton = document.getElementById(`categoryActivityButton`);
 categoryActivityButton.addEventListener(`click`, getActivityBasedOnCategory);
 
 // ---------- BONUS: PRICE RANGES ----------
+
+// Creating a function that is called if the network is done and there are no errors
+function priceRangeSuccess(res) {
+
+    // Storing the returned JavaScript object into a variable
+    let priceRangeActivities = res.data;
+
+    // Getting the p tag that will be manipulated to print messages to the user
+    let priceRangeActivityResult = document.getElementById(`priceRangeActivityResult`);
+    
+    // Storing the user's minimum and maximum values as variables
+    let minPrice = document.getElementById(`minPrice`).value;
+    let maxPrice = document.getElementById(`maxPrice`).value;
+
+    // If the user enters a valid price range, print the activity to the user
+    if (minPrice < maxPrice) {
+        priceRangeActivityResult.innerText = priceRangeActivities.activity;
+    } 
+    
+    // If the user enters a valid price range but returned data does not have an activity at the that price range, print an error message to the user
+    else if (priceRangeActivities.price === undefined) {
+        priceRangeActivityResult.innerText = `No activity found at this price range. Please enter another value.`;
+    }
+
+    // If the user has not entered a minimum or maximum price, prompt the user to enter a minimum and maxmimum value.
+    if (minPrice === "" && maxPrice === "") {
+        priceRangeActivityResult.innerText = `Please enter a minimum and maximum price range.`;
+    }
+
+    // If the user entered a maximum price but not a minimum price, set the minimum price to 0 and print the activity between 0 and the user's maximum input price
+    else if (minPrice === "") {
+        minPrice = 0;
+        priceRangeActivityResult.innerText = priceRangeActivities.activity;
+    } 
+    
+    // If the user entered a minimum price but not a maximum price, set the maximum price to 1 and print the activity between the user's minimum input price and 1
+    else if (maxPrice === "") {
+        maxPrice = 1;
+        priceRangeActivityResult.innerText = priceRangeActivities.activity;
+    }
+
+    // If the minimum price or maximum price is a negative number or the minimum price is a negative but the maximum price is greater than or equal to 0, print a error message to the user
+    if (minPrice < 0 || maxPrice < 0 || (minPrice < 0 && maxPrice >= 0)) {
+        priceRangeActivityResult.innerText = `Invalid price value. Please enter another value.`;
+    }
+
+    // If the minimum price or maximum price is greater than the maximum price range of 1, print an error message to the user
+    else if (minPrice > 1 || maxPrice > 1) {
+        priceRangeActivityResult.innerText = `The input value has exceeded the maximum price range. Please enter another value.`;
+    }
+}
+
+// Creating a function that is called if the page errors, printing an error message to the user
+function priceRangeFailure(err) {
+    priceRangeActivityResult.innerText = `Sorry, something went wrong. Failed to recieve activity. Please try again.`;
+}
+
+// Creating a function that is called when the user clicks on the button
+function getActivityBasedOnPriceRange(e) {
+
+    // Storing the user's minimum price and maximum price input value as variables
+    let minPrice = document.getElementById(`minPrice`).value;
+    let maxPrice = document.getElementById(`maxPrice`).value;
+
+    // When the button is clicked, configure the request with the type and URL with the value of the parameters being the minimum and maximum price range the user has entered
+    // If the request doesn't fail, it will call the priceRangeSuccess function
+    // If the request fails, it will call the priceRangeFailure function
+    axios.request({
+        method: `GET`,
+        url: `http://www.boredapi.com/api/activity`,
+        params: {
+            minprice: minPrice,
+            maxprice: maxPrice
+        }
+    }).then(priceRangeSuccess).catch(priceRangeFailure);
+}
+
+// Adding a click event to the button and calling the function that will send a GET request and print the activity to the user based on the user's minimum and maximum price range
+let priceRangeActivityButton = document.getElementById(`priceRangeActivityButton`);
+priceRangeActivityButton.addEventListener(`click`, getActivityBasedOnPriceRange);
+
+// ---------- BONUS: ACCESSIBILITY VALUES ----------
+
+// Creating a function that will print the accessibility value the user has selected when they move the slider
+function showAccessibilitySelected(e) {
+    document.getElementById(`accessibility`).innerText = `Accessibility: ${accessibilitySelected.value}`;
+}
+
+// // Creating a function that is called if the network is done and there are no errors
+function accessibilitySuccess(res) {
+
+    // Storing the returned JavaScript object into a variable
+    let accessibilityActivities = res.data;
+
+    // Printing the activity to the user based on the accessibility value the user has selected
+    let result = document.getElementById(`accessibilityActivityResult`);
+    result.innerText = accessibilityActivities.activity;
+
+    // If returned data does not have an activity at the selected accessibility value, print an error message to the user
+    if (accessibilityActivities.price === undefined) {
+        result.innerText = `No activity found at this accessibility value. Please select another value.`;
+    }
+
+    // Since the default value of the slider is 0.5, the showAccessibilitySelected function will be called to print the accessibility default value even if the user clicks on the button and has not selected an accessibility value
+    showAccessibilitySelected();
+}
+
+// Creating a function that is called if the page errors, printing an error message to the user
+function accessibilityFailure(err) {
+    document.getElementById(`accessibilityActivityResult`).innerText = `Sorry, something went wrong. Failed to recieve an activity. Please try again.`;
+}
+
+// Creating a function that is called when the user clicks on the button
+function getActivityBasedOnAccessibility(e) {
+
+    // When the button is clicked, configure the request with the type and URL with the value of the parameter being the accessibility value the user has selected
+    // If the request doesn't fail, it will call the priceRangeSuccess function
+    // If the request fails, it will call the priceRangeFailure function
+    axios.request({
+        method: `GET`,
+        url: `http://www.boredapi.com/api/activity`,
+        params: {
+            accessibility: accessibilitySelected.value
+        }
+    }).then(accessibilitySuccess).catch(accessibilityFailure);
+}
+
+// Adding a click event to the button and calling the function that will send a GET request and print the activity to the user based on the user's selected accessibility value
+let accessibilityActivityButton = document.getElementById(`accessibilityActivityButton`);
+accessibilityActivityButton.addEventListener(`click`, getActivityBasedOnAccessibility);
+
+// Adding a input event to the slider and calling the function that will print the selected accessibility values to the user
+let accessibilitySelected = document.getElementById(`accessibilityInput`);
+accessibilitySelected.addEventListener(`input`, showAccessibilitySelected);
